7ecc851acb8aab10962d4317735613a785b6e231
KaptainCS3 <mbiapplegryn@gmail.com>
2023-05-11 15:19:04 +0100

    initial commit

---
+++ Travel_Xexpress/src/components/About.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <section className="m-8 py-8 bg-white shadow-2xl px-6">
+        <h3>About Us</h3>
+        <h2 className="hero">We Provide Best Tour Packages In Your Budget</h2>
+        <p>
+          Dolores lorem lorem ipsum sit et ipsum. Sadip sea amet diam dolore sed
+          et. Sit rebum labore sit sit ut vero no sit. Et elitr stet dolor sed
+          sit et sed ipsum et kasd ut. Erat duo eos et erat sed diam duo
+        </p>
+        <div>
+          <div>
+            <img />
+          </div>
+          <div>
+            <img />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default About;
