4dfe3a45591395151e14e0f8439165584d1ba651
KaptainCS3 <mbiapplegryn@gmail.com>
2023-05-11 15:19:15 +0100

    initial commit

---
+++ Travel_Xexpress/src/components/Tourist.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Tourist = () => {
+  return (
+    <>
+      <section className="px-8 py-24">
+        <img src="../../public/assets/about.jpg" alt="tourist image" />
+      </section>
+    </>
+  );
+};
+
+export default Tourist;
