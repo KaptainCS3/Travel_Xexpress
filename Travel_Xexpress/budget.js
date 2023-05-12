b5cdf065381de05df2fa31db2086ede7798263a6
KaptainCS3 <mbiapplegryn@gmail.com>
2023-05-12 22:32:26 +0100

    initial commit

---
+++ Travel_Xexpress/budget.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: 1,
+    src: "faMoneyCheckAlt",
+    title: "Competitive Pricing",
+    description:
+      "Magna sit magna dolor duo dolor labore rebum amet elitr est diam sea",
+  },
+  {
+    id: 2,
+    src: "faMoneyCheckAlt",
+    title: "Best Services",
+    description:
+      "Magna sit magna dolor duo dolor labore rebum amet elitr est diam sea",
+  },
+  {
+    id: 3,
+    src: "faMoneyCheckAlt",
+    title: "Worldwide Coverage",
+    description:
+      "Magna sit magna dolor duo dolor labore rebum amet elitr est diam sea",
+  },
+];
