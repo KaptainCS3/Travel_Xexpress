f2b3a69bdbf7e560e7418e318f6bd373de8f6ea0
KaptainCS3 <mbiapplegryn@gmail.com>
2023-05-12 22:32:41 +0100

    initial commit

---
+++ Travel_Xexpress/explore.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    id: 1,
+    tittle: "United States",
+    content: "100 Cities",
+    images: "/assets/destination-1.jpg",
+  },
+  {
+    id: 2,
+    tittle: "United Kingdom",
+    content: "100 Cities",
+    images: "/assets/destination-2.jpg",
+  },
+  {
+    id: 3,
+    tittle: "Australia",
+    content: "100 Cities",
+    images: "/assets/destination-3.jpg",
+  },
+  {
+    id: 4,
+    tittle: "India",
+    content: "100 Cities",
+    images: "/assets/destination-4.jpg",
+  },
+  {
+    id: 4,
+    tittle: "South Africa",
+    content: "100 Cities",
+    images: "/assets/destination-5.jpg",
+  },
+  {
+    id: 4,
+    tittle: "Indonesia",
+    content: "100 Cities",
+    images: "/assets/destination-6.jpg",
+  },
+];
