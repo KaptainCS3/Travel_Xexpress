99f4d5771f902e1bf405c911f2abbd2791dc5386
KaptainCS3 <mbiapplegryn@gmail.com>
2023-05-12 22:33:55 +0100

    update component

--- Travel_Xexpress/carouselList.js
+++ Travel_Xexpress/carouselList.js
@@ -1,11 +1,11 @@
 export default [
   {
-    imageSrc: "assets/carousel-1.jpg",
+    imageSrc: "/assets/carousel-1.jpg",
     title: "tour & travel",
     content: "Let's Discover The World Together",
   },
   {
-    imageSrc: "assets/carousel-2.jpg",
+    imageSrc: "/assets/carousel-2.jpg",
     title: "tour & travel",
     content: "Discover Amazing Places With Us",
   },
