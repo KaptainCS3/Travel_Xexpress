ea155d85173eeded56357cc2222414ff0a7a4d71
KaptainCS3 <mbiapplegryn@gmail.com>
2023-05-16 22:26:10 +0100

    update file

--- Travel_Xexpress/src/components/Package.jsx
+++ Travel_Xexpress/src/components/Package.jsx
@@ -156,7 +156,7 @@ const Package = () => {
                         <small className="text-[#656565]">{item.numBook}</small>
                       </div>
                     </div>
-                    <h3 className="text-[#212121] text-xl font-medium mb-4">
+                    <h3 className="text-[#212121] text-xl font-medium mb-4 hover:text-[#7AB730]">
                       {item.description}
                     </h3>
                     <div className="flex justify-between border-t-[0.05rem] pt-4">
