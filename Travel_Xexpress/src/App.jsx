c017e73d58f13eccc5654f22e9a561de7d22f3d6
KaptainCS3 <mbiapplegryn@gmail.com>
2023-05-17 19:54:54 +0100

    update App

--- Travel_Xexpress/src/App.jsx
+++ Travel_Xexpress/src/App.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import Nav from './components/Nav'
-import './App.css'
-import Hero from './components/Hero'
-import BookTravel from './components/BookTravel'
-import Tourist from './components/Tourist'
-import About from './components/About'
-import Budget from './components/Budget'
-import Explore from './components/Explore'
-import Service from './components/Service'
-import PackageTour from './components/Package'
-import Offer from './components/Offer'
-import Guide from './components/Guide'
+import React from "react";
+import Nav from "./components/Nav";
+import "./App.css";
+import Hero from "./components/Hero";
+import BookTravel from "./components/BookTravel";
+import Tourist from "./components/Tourist";
+import About from "./components/About";
+import Budget from "./components/Budget";
+import Explore from "./components/Explore";
+import Service from "./components/Service";
+import PackageTour from "./components/Package";
+import Offer from "./components/Offer";
+import Guide from "./components/Guide";
+import BlogPost from "./components/BlogPost";
 const App = () => {
   return (
     <>
@@ -26,9 +27,10 @@ const App = () => {
         <PackageTour />
         <Offer />
         <Guide />
+        <BlogPost />
       </div>
     </>
   );
-}
+};
 
-export default App
+export default App;
