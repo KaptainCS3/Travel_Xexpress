9b349ca1cba173f6d8f874ea2fa1d311f7a9c457
KaptainCS3 <mbiapplegryn@gmail.com>
2023-05-17 23:22:38 +0100

    initial commit

---
+++ Travel_Xexpress/client.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    id: 1,
+    src: "/assets/testimonial-1.jpg",
+    title: "Client name",
+    description:
+      "Dolor et eos labore, stet justo sed est sed. Diam sed sed dolor stet amet eirmod eos labore diam",
+    profession: "Profession",
+  },
+  {
+    id: 2,
+    src: "/assets/testimonial-2.jpg",
+    title: "Client name",
+    description:
+      "Dolor et eos labore, stet justo sed est sed. Diam sed sed dolor stet amet eirmod eos labore diam",
+    profession: "Profession",
+  },
+  {
+    id: 3,
+    src: "/assets/testimonial-3.jpg",
+    title: "Client name",
+    description:
+      "Dolor et eos labore, stet justo sed est sed. Diam sed sed dolor stet amet eirmod eos labore diam",
+    profession: "Profession",
+  },
+  {
+    id: 4,
+    src: "/assets/testimonial-4.jpg",
+    title: "Client name",
+    description:
+      "Dolor et eos labore, stet justo sed est sed. Diam sed sed dolor stet amet eirmod eos labore diam",
+    profession: "Profession",
+  },
+];
