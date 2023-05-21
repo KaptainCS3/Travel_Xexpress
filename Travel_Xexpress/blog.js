33ecf5173439544312224a3b7ac9d7260fd66d0d
KaptainCS3 <mbiapplegryn@gmail.com>
2023-05-21 23:38:35 +0100

    update directory

---
+++ Travel_Xexpress/blog.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: 1,
+    src: "/assets/blog-1.jpg",
+    title: "ADMIN",
+    title_: "TOURS & TRAVEL",
+    description: "Dolor justo sea kasd lorem clita justo diam amet",
+  },
+  {
+    id: 2,
+    src: "/assets/blog-2.jpg",
+    title: "ADMIN",
+    title_: "TOURS & TRAVEL",
+    description: "Dolor justo sea kasd lorem clita justo diam amet",
+  },
+  {
+    id: 3,
+    src: "/assets/blog-3.jpg",
+    title: "ADMIN",
+    title_: "TOURS & TRAVEL",
+    description: "Dolor justo sea kasd lorem clita justo diam amet",
+  },
+];
