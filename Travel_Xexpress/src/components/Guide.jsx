2b1fda873770abeede10e6510871feab0a968064
KaptainCS3 <mbiapplegryn@gmail.com>
2023-06-09 20:51:24 +0100

    update component

--- Travel_Xexpress/src/components/Guide.jsx
+++ Travel_Xexpress/src/components/Guide.jsx
@@ -1,3 +1,10 @@
+import {
+  faFacebookF,
+  faInstagram,
+  faLinkedinIn,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 const Guide = () => {
@@ -39,10 +46,24 @@ const Guide = () => {
               return (
                 <div
                   key={item.id}
-                  className="mb-8 flex w-full flex-col justify-center items-center hover:shadow-2xl bg-white srv transition-all duration-500 delay-0 ease-linear"
+                  className="guide cursor-pointer mb-8 flex w-full flex-col justify-center items-center hover:shadow-2xl bg-white srv transition-all duration-500 delay-0 ease-linear"
                 >
-                  <div className="transition-all duration-500 delay-0 ease-linear">
-                    <img src={item.src} alt="" className="h-full" />
+                  <div className="relative overflow-hidden transition-all duration-500 delay-0 ease-linear">
+                    <img src={item.src} alt="" className="h-auto max-w-full" />
+                    <div className="social-icon absolute h-full flex justify-between items-center px-16 w-full top-0 left-0">
+                      <div className="cursor-pointer w-9 h-9 border border-[#7AB730] text-[#7AB730] text-xl flex items-center justify-center hover:bg-[#7AB730] hover:text-white">
+                        <FontAwesomeIcon icon={faTwitter} />
+                      </div>
+                      <div className="cursor-pointer w-9 h-9 border border-[#7AB730] text-[#7AB730] text-xl flex items-center justify-center hover:bg-[#7AB730] hover:text-white">
+                        <FontAwesomeIcon icon={faFacebookF} />
+                      </div>
+                      <div className="cursor-pointer w-9 h-9 border border-[#7AB730] text-[#7AB730] text-xl flex items-center justify-center hover:bg-[#7AB730] hover:text-white">
+                        <FontAwesomeIcon icon={faInstagram} />
+                      </div>
+                      <div className="cursor-pointer w-9 h-9 border border-[#7AB730] text-[#7AB730] text-xl flex items-center justify-center hover:bg-[#7AB730] hover:text-white">
+                        <FontAwesomeIcon icon={faLinkedinIn} />
+                      </div>
+                    </div>
                   </div>
                   <div className="my-6">
                     <h4 className="text-xl font-medium">{item.uName}</h4>
