b52cff5b3ffd1723b5acedcb922cf95773c3e90b
KaptainCS3 <mbiapplegryn@gmail.com>
2023-06-21 18:19:31 +0100

    initial commit

---
+++ Travel_Xexpress/src/components/AboutMe.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Tourist from "../components/Tourist";
+import About from "../components/About";
+const AboutMe = () => {
+  return (
+    <div className="lg:relative lg:w-[82.5%] lg:mx-auto lg:mt-32 lg:flex lg:min-h-[100vh]">
+      <Tourist />
+      <About />
+    </div>
+  );
+};
+
+export default AboutMe;
