0b6d67975faf8e2e6141eef074265848fb1b4e74
KaptainCS3 <mbiapplegryn@gmail.com>
2023-06-28 20:54:40 +0100

    initial commit

---
+++ Travel_Xexpress/src/components/CommentList.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const CommentList = () => {
+  return (
+    <div className="flex">
+      <div className="mr-4 mt-2">
+        <img
+          src="../../public/assets/user.jpg"
+          alt="John Doe"
+          className="w-12 h-auto"
+        />
+      </div>
+      <div className="w-[93%]">
+        <span>
+          <span className="hover:underline text-[#7AB730] cursor-pointer pr-1">
+            John Doe{" "}
+          </span>
+          <span className="italic text-[0.8rem]">01 Jan 2045</span>
+        </span>
+        <p className="text-[#656565] pb-3">
+          Diam amet duo labore stet elitr invidunt ea clita ipsum voluptua,
+          tempor labore accusam ipsum et no at. Kasd diam tempor rebum magna
+          dolores sed sed eirmod ipsum. Gubergren clita aliquyam consetetur
+          sadipscing, at tempor amet ipsum diam tempor consetetur at sit.
+        </p>
+        <div>
+          <button className="border border-[#7AB730] text-[0.8rem] px-2 py-[0.25rem] text-[#7AB730] hover:bg-[#7AB730] hover:text-white">
+            Reply
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CommentList
