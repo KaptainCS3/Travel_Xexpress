e7572e3d68dd414004c9cd0ab660a0a3136da762
KaptainCS3 <mbiapplegryn@gmail.com>
2023-06-29 22:10:18 +0100

    complete component

--- Travel_Xexpress/src/pages/Services.jsx
+++ Travel_Xexpress/src/pages/Services.jsx
@@ -1,26 +1,29 @@
-import React from 'react'
-import Nav from '../components/Nav';
-import Service from '../components/Service';
-import Footer from '../components/Footer';
-const Services = () => {
+import React from "react";
+import Nav from "../components/Nav";
+import Header from "../components/Header";
+import BookTravel from "../components/BookTravel";
+import Service from "../components/Service";
+import ServicesCard from "../components/ServicesCard";
+import ScrollTop from "../components/ScrollTop";
+import Footer from "../components/Footer";
+const Services = ({ header }) => {
   return (
     <div className="bg-[#F3F3F3] font">
       <Nav />
-      <div className="mb-32">
-        {/* <AboutMe /> */}
+      <Header header={header} />
+      <BookTravel />
+      <div className="my-32">
+        <Service />
+        <section className="lg:w-[82.5%] mx-auto">
+          <ServicesCard />
+        </section>
       </div>
-      <div className="mb-32">
-        <Service/>
-      </div>
-      <div className="mb-32">
-        {/* <Offer /> */}
-      </div>
-      <div className="mb-24">
-        {/* <Guide /> */}
-      </div>
+      <div className="mb-32">{/* <Offer /> */}</div>
+      <div className="mb-24">{/* <Guide /> */}</div>
+      <ScrollTop />
       <Footer />
     </div>
   );
-}
+};
 
-export default Services
+export default Services;
