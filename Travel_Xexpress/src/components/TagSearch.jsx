023f076d69a1ef7d7bd8d9f2ab562517359da180
KaptainCS3 <mbiapplegryn@gmail.com>
2023-06-29 22:11:15 +0100

    complete component

--- Travel_Xexpress/src/components/TagSearch.jsx
+++ Travel_Xexpress/src/components/TagSearch.jsx
@@ -9,12 +9,12 @@ const TagSearch = () => {
             tag cloud
           </h2>
           <div className="self-start p-0 m-0">
-            <button className="bg-white py-2 px-3 my-2">Design</button>
-            <button className="bg-white py-2 px-3 m-2">Development</button>
-            <button className="bg-white py-2 px-3 mr-2">Marketing</button>
-            <button className="bg-white py-2 px-3 my-2">SEO</button>
-            <button className="bg-white py-2 px-3 m-2">Writing</button>
-            <button className="bg-white py-2 px-3 mr-2">Consulting</button>
+            <button className="bg-white py-2 px-3 my-2 hover:bg-[#ececec] hover:border-[#e6e6e6] border-[#fff] border">Design</button>
+            <button className="bg-white py-2 px-3 m-2 hover:bg-[#ececec] hover:border-[#e6e6e6] border-[#fff] border">Development</button>
+            <button className="bg-white py-2 px-3 mr-2 hover:bg-[#ececec] hover:border-[#e6e6e6] border-[#fff] border">Marketing</button>
+            <button className="bg-white py-2 px-3 my-2 hover:bg-[#ececec] hover:border-[#e6e6e6] border-[#fff] border">SEO</button>
+            <button className="bg-white py-2 px-3 m-2 hover:bg-[#ececec] hover:border-[#e6e6e6] border-[#fff] border">Writing</button>
+            <button className="bg-white py-2 px-3 mr-2 hover:bg-[#ececec] hover:border-[#e6e6e6] border-[#fff] border">Consulting</button>
           </div>
         </section>
       </div>
