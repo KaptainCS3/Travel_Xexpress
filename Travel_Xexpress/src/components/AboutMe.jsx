1cc738a04b6ccb09678f0dc942ff142c21cd0894
KaptainCS3 <mbiapplegryn@gmail.com>
2023-08-01 14:24:41 +0100

    deploy

--- Travel_Xexpress/src/components/AboutMe.jsx
+++ Travel_Xexpress/src/components/AboutMe.jsx
@@ -3,7 +3,7 @@ import Tourist from "../components/Tourist";
 import About from "../components/About";
 const AboutMe = () => {
   return (
-    <div className="lg:relative lg:w-[82.5%] lg:mx-auto lg:mt-32 lg:flex lg:min-h-[100vh]">
+    <div className="lg:relative lg:w-[82.5%] lg:mx-auto lg:mt-32 lg:flex lg:min-h-[100vh] md:mx-8">
       <Tourist />
       <About />
     </div>
