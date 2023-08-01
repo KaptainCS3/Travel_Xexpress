dfd1d97c0174636c55b2055637d74b6c01e4c3fc
KaptainCS3 <mbiapplegryn@gmail.com>
2023-08-01 14:24:27 +0100

    deploy

--- Travel_Xexpress/src/main.jsx
+++ Travel_Xexpress/src/main.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import AboutUs from "./pages/AboutUs";
+import Services from "./pages/Services";
+import Packages from "./pages/Packages";
+import Contacts from "./pages/Contacts";
+import Blog from "./pages/Blog";
+import Destinations from "./pages/Destinations";
+import Guides from "./pages/Guides";
+import Testimonials from "./pages/Testimonials";
+import BlogDetails from "./pages/BlogDetails";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<AboutUs header="about" />} />
+        <Route path="/services" element={<Services header="services" />} />
+        <Route path="/package" element={<Packages header="packages" />} />
+        <Route path="/contact" element={<Contacts header="contact" />} />
+        <Route path="/blog" element={<Blog header="blog" />} />
+        <Route
+          path="/destination"
+          element={<Destinations header="destination" />}
+        />
+        <Route path="/guide" element={<Guides header="guides" />} />
+        <Route
+          path="/testimonial"
+          element={<Testimonials header="testimonials" />}
+        />
+        <Route
+          path="/blog detail"
+          element={<BlogDetails header="blog details" />}
+        />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
