3c1681d85a76482a6514b3c85f74d68195f38a8f
KaptainCS3 <mbiapplegryn@gmail.com>
2023-08-02 23:02:26 +0100

    fix image path

--- Travel_Xexpress/src/components/Tourist.jsx
+++ Travel_Xexpress/src/components/Tourist.jsx
@@ -4,7 +4,7 @@ const Tourist = () => {
   return (
     <>
       <section className="px-8 py-24 lg:mt-16 lg:p-0 lg:relative lg:w-[48%] lg:h-full">
-        <img src="../../public/assets/about.jpg" alt="tourist image" className="h-full"/>
+        <img src="/assets/about.jpg" alt="tourist image" className="h-full"/>
       </section>
     </>
   );
